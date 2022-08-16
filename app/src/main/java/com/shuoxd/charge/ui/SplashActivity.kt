214ef5fa5273b3f7d675b45bcf774868dec3eb59
JashonL <721695214@qq.com>
2022-08-16 23:17:52 +0800

    Initial commit

---
+++ app/src/main/java/com/shuoxd/charge/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.shuoxd.charge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shuoxd.charge.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
